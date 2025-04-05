749f9531d2d98e8daf7b79b8b714e4849ce90f66
khoideptra04 <khoiga129@gmail.com>
2025-04-05 16:01:29 +0700

    5/4nopbai

--- App.js
+++ App.js
@@ -1,25 +1,7 @@
+// App.js
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import SelectLocation from './src/screens/SelectLocation';
-import Login from './src/screens/Login';
-import SignUp from './src/screens/SignUp';
-
-const Stack = createNativeStackNavigator();
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="SelectLocation"
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="SelectLocation" component={SelectLocation} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppNavigator />;
 }
--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-Họ và Tên : Lê Nguyên Khôi
-Mã sv : 22810310244
-
-Output:
-
-![image](https://github.com/user-attachments/assets/40b2fa95-28a2-49af-aaeb-a64bdb2a8ff2)
-![image](https://github.com/user-attachments/assets/3e4017ba-e86e-41c3-984e-79cabdf372c7)
-![image](https://github.com/user-attachments/assets/1e6c0e2a-210b-4007-b8d9-6570ffe22c1e)
+Lê Nguyên Khôi- 22810310244
+<img src ='./anh1.jpg' alt="Screenshot" width = "300">
+<img src ='./anh2.jpg' alt="Screenshot" width = "300">
+<img src ='./anh3.jpg' alt="Screenshot" width = "300">
+<img src ='./anh4.jpg' alt="Screenshot" width = "300">
